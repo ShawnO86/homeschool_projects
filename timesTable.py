efbaba368711b8bc5ef95afff5cd220fca4d12cf
Shawn Osborne <shawnby222@gmail.com>
2024-03-06 14:52:51 -0600

    changes recursion to for loop

--- timesTable.py
+++ timesTable.py
@@ -1,40 +1,38 @@
 def create_row(start, end):
     '''Creates times table row from start to end,
     returns list of integers from index 1 to end.'''
-    #init row with 1s, extra element added for index 0.
+    #init row with 1s, extra element added to account for index 0.
     row = [1 for _ in range(end + 1)]
     #loop for row length.
     for i in range(1, end + 1):
-        #set each value by multiplying
-        row[i] = str(start * i)
-        if len(row[i]) < 2:
-            row[i] += ' ' 
+        #set each value by multiplying, and right align number with 3 spaces
+        row[i] = str(start * i).rjust(3)
     return row[1:]
 
-def create_cols(start, end, cols = {}):
-    '''Recursivly creates times table from each create_row() call, 
-    returns dictionary with a times table row multiplier as each key(1s, 2s, 3s, etc...)'''
-    #base case returns if start meets end value
-    if end < start:
-        return
-    else:
-        #adds row to cols dictionary
-        cols[str(start) + "s"] = create_row(start, end)
-        #calls function again with updated 'columns'
-        create_cols(start + 1, end, cols)
-    return cols
+def create_table(start, end):
+    '''Creates aa times table from start to end values.'''
+    times_table = {}
+    for i in range(start, end + 1):
+        times_table[str(i) + 's'] = create_row(i, end)
+    return times_table
+
+def get_input(value):
+    while True:
+        try:
+            val = int(input(value))
+            return val
+        except ValueError:
+            print('Value must be a number!')
 
 
 if __name__ == "__main__":
-    rows = int(input('Starting value: \n'))
-    cols = int(input('Ending value: \n'))
-    table = create_cols(rows, cols)
+    start = get_input('Starting value: ')
+    end = get_input('Ending value: ')
+    table = create_table(start, end)
     txtFile = 'timesTable.txt'
-    file = open(txtFile, 'w')
-    for key in table:
-        line = ' | '.join(table[key])
-        print(line)
-        print()
-        file.write(line)
-        file.write('\n\n')
-    file.close()
+
+    with open(txtFile, 'w') as file:
+        for key in table:
+            line = ' | '.join(table[key]) + '\n'
+            print(line)
+            file.write(line)

06d1ab4dbc1aefdd03616fe6c35bfe2bf4180fa0
Shawn Osborne <shawnby222@gmail.com>
2024-05-06 19:04:27 -0500

    Starts code seperation

---
+++ timeTableProject/problems.js
@@ -0,0 +1,6 @@
+/* TO DO:
+Mess around with building 2+ digit problems using HTML forms,
+Find way to validate each part (ones, tens, carry overs, etc..)
+
+*/
+
---
+++ timeTableProject/timesTable.js
@@ -0,0 +1,29 @@
+function createTimesTable() {
+    const tableSection = document.getElementById('timesTable');
+    for (let i = 1; i <= 10; i++){
+        const col = document.createElement('div');
+        tableSection.appendChild(col);
+        col.classList.add('col_'+i);
+    
+        for (let j = 1; j <= 10; j++){
+            const newDiv = document.createElement('div');
+            col.appendChild(newDiv);
+            newDiv.classList.add('row_' + j);
+            newDiv.classList.add('cell');
+            newDiv.innerText = i * j;
+        };
+    };
+    
+    const cells = document.querySelectorAll('.cell');
+    const formulaOut = document.getElementById('tableFormula');
+    
+    cells.forEach((cell) => {
+        cell.addEventListener('mousedown', (e) => {
+            let row = e.target.classList[0];
+            let col = e.target.parentElement.classList[0];
+            formulaOut.innerText = row.slice(4) +" x "+ col.slice(4) +" = " + e.target.innerText;
+        });
+    });
+};
+
+createTimesTable();

24db9243f2eb20ee71873f50b68c2760cd1dfef8
Shawn Osborne <shawnby222@gmail.com>
2024-05-07 15:23:12 -0500

    adds extra comments to createProblem()

--- timeTableProject/problems.js
+++ timeTableProject/problems.js
@@ -1,31 +1,21 @@
-/* TO DO:
-Mess around with building 2+ digit problems using HTML forms,
-Find way to validate each part (ones, tens, carry overs, etc..)
-
-*/
-
-/* <form class="multiply">
-    <input type="text" class="carry">
-        <div class="defProblem">
-            <p><br>x</p>
-            <p>48 <br> 15</p>
-        </div>
-    <input type="text" class="ones solution">
-    <input type="text" class="tens solution">
-</form> */
 
 function createProblem() {
-    //Math.random 0 inclusive to 1 exclusive runs first then regular order of operations on math then takes the floor of the result.
+    //generate two random numbers between 10 and 99
     const num1 = Math.floor(Math.random() * 90 + 10);
     const num2 = Math.floor(Math.random() * 90 + 10);
+
     //multiplies num1 by ones digit of num2
     const onesAnswer = num1 * (num2 % 10);
-    //calculates carry over value for onesAnswer by multiplying ones digit in each num. then, if there is a value with a tens digit, that is the carry value, otherwise its 0.
-    const onesCarry = Math.floor((((num1 % 10) * (num2 % 10)) / 10) % 10);
+    //calculates carry over value -- (num1 % 10) * (num2 % 10) selects the ones place digits and multiplies them,
+    //dividing that product by 10 moves decimal to the tens place, Math.floor() ensures it's an integer by removing everything after decimal.
+    const onesCarry = Math.floor((((num1 % 10) * (num2 % 10)) / 10));
+
     //multiplies num1 by tens digit of num2 and mulitplies result by 10 
     const tensAnswer = (num1 * Math.floor(num2 / 10)) * 10;
+
     //sums onesAnswers and tensAnswers
     const finalAnswer = onesAnswer + tensAnswer;
+
     return [num1, num2, onesAnswer, tensAnswer, finalAnswer, onesCarry];
 };
 
@@ -62,6 +52,10 @@ function writeProblemArr(problems) {
     });
 };
 
+/* TO DO:
+Mess around with building 2+ digit problems using HTML forms,
+Find way to validate each part (ones, tens, carry overs, etc..)
+*/
 function validateProblems() {
     const problemSection = document.getElementById("problems");
 
@@ -69,10 +63,3 @@ function validateProblems() {
 
 
 writeProblemArr(createProblemArr());
-
-const num1 = Math.floor(Math.random() * 90 + 10);
-const num2 = Math.floor(Math.random() * 90 + 10);
-const onesAnswer = num1 * (num2 % 10);
-const onesCarry = Math.floor((((num1 % 10) * (num2 % 10)) / 10) % 10);
-
-console.log(num1, num2, onesCarry)
